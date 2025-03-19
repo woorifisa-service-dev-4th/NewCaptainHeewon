3be26e95996e1365f6a62361ef9ab4cc0b67aec3
이소연 <red426@sookmyung.ac.kr>
2025-03-19 15:15:36 +0900

    security config 수정

--- src/main/java/com/guard/config/SecurityConfig.java
+++ src/main/java/com/guard/config/SecurityConfig.java
@@ -2,15 +2,20 @@ package com.guard.config;
 
 
 
+import java.nio.charset.StandardCharsets;
+import java.security.interfaces.RSAPublicKey;
 import java.util.List;
 
 import org.apache.catalina.filters.CorsFilter;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
@@ -18,6 +23,8 @@ import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 
 import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.models.OpenAPI;
+
+import javax.crypto.spec.SecretKeySpec;
 
 @Configuration
 public class SecurityConfig {
@@ -31,6 +38,11 @@ public class SecurityConfig {
 			.and()
 			.oauth2ResourceServer().jwt();
 		return http.build();
+	}
+	@Bean
+	public JwtDecoder jwtDecoder(@Value("${jwt.secret}") String secretKey) {
+		SecretKeySpec keySpec = new SecretKeySpec(secretKey.getBytes(StandardCharsets.UTF_8), "HMACSHA256");
+		return NimbusJwtDecoder.withSecretKey(keySpec).build();
 	}
 
 	@Bean
